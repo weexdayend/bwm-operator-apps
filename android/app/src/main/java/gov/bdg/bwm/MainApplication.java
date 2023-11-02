a81237b1d6e17549050dec61b46b9ac54cadddbd
weexdayend <weexdayend@gmail.com>
2023-11-02 13:18:29 +0700

    bwm apps

--- android/app/src/main/java/gov/bdg/bwm/MainApplication.java
+++ android/app/src/main/java/gov/bdg/bwm/MainApplication.java
@@ -1,4 +1,4 @@
-package com.bwmapp;
+package gov.bdg.bwm;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/BWMappTests/BWMappTests.m
+++ ios/BWMappTests/BWMappTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BWMappTests : XCTestCase
+@interface BWMAppTests : XCTestCase
 
 @end
 
-@implementation BWMappTests
+@implementation BWMAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
